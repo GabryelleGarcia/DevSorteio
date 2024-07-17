5295c8d235951e74761ee8692c98320e6c755127
GabryelleGarcia <gabygacruz@hotmail.com>
2024-07-17 11:02:55 -0300

    Finalização de código, ajuste de alguns detalhes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function generateNumber(){
 
-const min = document.querySelector(".input.min").value
-const max = document.querySelector(".input.max").value
+const min = Math.ceil (document.querySelector(".input-min").value)
+const max = Math.floor (document.querySelector(".input-max").value)
+
+const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+alert(result)
 }
